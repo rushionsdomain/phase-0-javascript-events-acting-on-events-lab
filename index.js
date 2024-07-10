7afb45d985fd2f486da7ddcf89f1a2d30150b234
Rushion <rushionchegge@gmail.com>
2024-07-10 15:07:53 +0300

    added index.js file

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Your code here
+// Grab the dodger element
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+// Function to move the dodger to the right
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+// Add an event listener
+document.addEventListener("keydown", function (event) {
+  if (event.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (event.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
