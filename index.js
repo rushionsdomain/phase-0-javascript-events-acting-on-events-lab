f41432a4e63527b4c7e579b59b3273acb33af1dd
Rushion <rushionchegge@gmail.com>
2024-07-22 11:46:27 +0300

    added js file that moves DODGER to the left & right

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Your code here
+function moveDodgerLeft() {
+  const dodger = document.getElementById("dodger");
+  let left = dodger.style.left.replace("px", "");
+  left = parseInt(left, 10);
+
+  // Move the dodger 4px to the left
+  dodger.style.left = `${left - 4}px`;
+}
+
+function moveDodgerRight() {
+  // Get the current position of the dodger
+  const dodger = document.getElementById("dodger");
+  let left = dodger.style.left.replace("px", "");
+  left = parseInt(left, 10);
+
+  // Move the dodger 4px to the right
+  dodger.style.left = `${left + 4}px`;
+}
